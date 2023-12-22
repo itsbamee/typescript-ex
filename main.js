0524f49ca3113790a52d70bf741a78f5fdaa37cd
itsbamme <jan9239@naver.com>
2023-12-22 21:44:46 +0900

    체크박스 선택하고 새로고침 시 스타일 유지

--- main.js
+++ main.js
@@ -9,27 +9,18 @@ let tasks = data ? JSON.parse(data) : [];
 
 tasks.forEach(task => addListItem(task));
 
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	if (input.value.trim() === '') return alert('오늘 할 일을 입력하세요.');
-
-	const newTask = {
-		id: performance.now(),
-		title: input.value,
-		createAt: new Date(),
-		complete: false
-	};
-
-	tasks.push(newTask);
-
-	input.value = '';
-	addListItem(newTask);
-});
-
 function addListItem(task) {
 	const item = document.createElement('li');
 	const checkbox = document.createElement('input');
 	checkbox.type = 'checkbox';
+
+	if (task.complete) {
+		item.style.textDecoration = 'line-through';
+		checkbox.checked = true;
+	} else {
+		item.style.textDecoration = 'none';
+		checkbox.checked = false;
+	}
 
 	checkbox.addEventListener('change', () => {
 		task.complete = checkbox.checked;
@@ -43,4 +34,12 @@ function addListItem(task) {
 
 function saveTasks() {
 	localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
+
+function init() {
+	tasks.forEach((task, idx) => {
+		list.querySelectorAll('li')[idx].querySelector('input').checked
+			? (list.querySelectorAll('li')[idx].style.textDecoration = 'line-through')
+			: (list.querySelectorAll('li')[idx].style.textDecoration = 'none');
+	});
 }

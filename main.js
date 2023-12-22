9d1d01a42f49c1dd3ad744edf435774bcc60b2a8
itsbamme <jan9239@naver.com>
2023-12-22 21:17:24 +0900

    performance.now() 메모추가

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', e => {
 	if (input.value.trim() === '') return alert('오늘 할 일을 입력하세요.');
 
 	const newTask = {
-		id: performance.now(),
+		id: performance.now(), //performance.now() : ms 단위로 타임스탬프 기록(실행시간 측정)
 		title: input.value,
 		createAt: new Date()
 	};

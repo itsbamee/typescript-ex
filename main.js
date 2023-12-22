fd509d79a76b47bbca3234cd8d1e6a64614faf2c
itsbamme <jan9239@naver.com>
2023-12-22 21:27:59 +0900

    동적으로 이벤트 연결해서 체크유무에 따라 complete값 변경

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
 const list = document.querySelector('#list');
 const form = document.querySelector('#form');
 const input = document.querySelector('#title');
+const tasks = [];
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
-	//value : input에 입력되는 문자
-	//input에 입력되는 문자 공백을 다 지웠을 때 '' 빈 문자열이면 alert창
 	if (input.value.trim() === '') return alert('오늘 할 일을 입력하세요.');
 
 	const newTask = {
-		id: performance.now(), //performance.now() : ms 단위로 타임스탬프 기록(실행시간 측정)
+		id: performance.now(),
 		title: input.value,
-		createAt: new Date()
+		createAt: new Date(),
+		complete: false
 	};
 
-	console.log(newTask);
+	//할 일 목록을 tasks라는 배열에 추가
+	tasks.push(newTask);
+
+	input.value = '';
+	addListItem(newTask);
 });
+
+function addListItem(task) {
+	const item = document.createElement('li');
+	const checkbox = document.createElement('input');
+	checkbox.type = 'checkbox';
+
+	//동적으로 생성되는 체크박스에 변경점이 생길 때 complete에 boolean값이 담기도록 이벤트 핸들러 미리 연결
+
+	checkbox.addEventListener('change', () => {
+		task.complete = checkbox.checked;
+		console.log(tasks);
+	});
+
+	item.append(checkbox, task.title);
+	list.append(item);
+}

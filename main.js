9d7d26e6621b0841fea2f2c9015f571c4a703652
itsbamme <jan9239@naver.com>
2023-12-23 09:35:30 +0900

    ToDoList 기능완성

--- main.js
+++ main.js
@@ -7,7 +7,24 @@ let data = localStorage.getItem('TASKS');
 //아래 구분에 배열.parse(data)가 오류구문이므로 아래쪽에 삼항연산자 처리
 let tasks = data ? JSON.parse(data) : [];
 
+//페이지 로딩 시 로컬저장소의 값을 가져와서 초기 목록을 출력해주는 메서드
 tasks.forEach(task => addListItem(task));
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	if (input.value.trim() === '') return alert('할일을 입력하세요.');
+
+	const newTask = {
+		id: performance.now(),
+		title: input.value,
+		createAt: new Date(),
+		complete: false
+	};
+	tasks.push(newTask);
+	addListItem(newTask);
+	saveTasks();
+	input.value = '';
+});
 
 function addListItem(task) {
 	const item = document.createElement('li');
@@ -36,6 +53,7 @@ function saveTasks() {
 	localStorage.setItem('TASKS', JSON.stringify(tasks));
 }
 
+/*
 function init() {
 	tasks.forEach((task, idx) => {
 		list.querySelectorAll('li')[idx].querySelector('input').checked
@@ -43,3 +61,4 @@ function init() {
 			: (list.querySelectorAll('li')[idx].style.textDecoration = 'none');
 	});
 }
+*/
